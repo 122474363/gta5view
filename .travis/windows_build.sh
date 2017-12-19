8b845ca7828f08bd2c04d0314295d1fbac1a4628
Syping <schiedelrafael@keppe.org>
2017-12-19 18:22:22 +0100

    travis script update

--- .travis/windows_build.sh
+++ .travis/windows_build.sh
@@ -6,6 +6,6 @@ echo "gta5view build version is $APPLICATION_VERSION" && \
 mkdir build && \
 mkdir assets && \
 cd build && \
-qmake-static DEFINES+=GTA5SYNC_BUILDTYPE_DEV "DEFINES+=GTA5SYNC_APPVER=\\\\\\\"$APPLICATION_VERSION\\\\\\\"" ../../gta5view.pro && \
+qmake-static DEFINES+=GTA5SYNC_BUILDTYPE_DEV "DEFINES+=GTA5SYNC_APPVER=\\\\\\\"$APPLICATION_VERSION\\\\\\\"" ../gta5view.pro && \
 make -j 4 && \
 cp -Rf release/*.exe $PROJECT_DIR/assets/
